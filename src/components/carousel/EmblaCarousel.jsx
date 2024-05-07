1f3fb172b8e6edc8b52ca277b938c44f087e0034
Gabriel Bustos <gabrielbustos0905@gmail.com>
2024-05-07 15:18:50 -0300

    More Info

--- src/components/carousel/EmblaCarousel.jsx
+++ src/components/carousel/EmblaCarousel.jsx
@@ -27,19 +27,21 @@ export default function EmblaCarousel() {
   const [emblaRef] = useEmblaCarousel({ loop: true }, [Autoplay()])
 
   return (
-    <div className="embla" ref={emblaRef}>
-      <div className='embla__container'>
-        {
-          IMAGES.map(({id, title, img, demo}) => (
-            <div className='embla__slide' key={id}>
-              <img src={img} alt="" className=''/>
-              <div className='absolute top-0 m-4 flex justify-center items-center gap-4'>
-                <p className='inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 rounded-3xl'>{title}</p>
-                <a href={demo} className='inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black'><LinkIcon /> Demo</a>
+    <div className='w-full md:w-1/2 flex justify-center md:justify-normal'>
+      <div className="embla" ref={emblaRef}>
+        <div className='embla__container'>
+          {
+            IMAGES.map(({id, title, img, demo}) => (
+              <div className='embla__slide' key={id}>
+                <img src={img} alt="" className=''/>
+                <div className='absolute top-0 m-4 flex justify-center items-center gap-4'>
+                  <p className='inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 rounded-3xl'>{title}</p>
+                  <a href={demo} className='inline-flex items-center justify-center gap-2 px-3 py-2 space-x-2 text-base text-white transition bg-gray-800 border border-gray-600 text-md hover:bg-gray-800 hover:border-gray-900 group max-w-fit rounded-xl hover:text-white focus:outline-none focus-visible:outline-none focus-visible:ring focus-visible:ring-white focus-visible:ring-offset-2 active:bg-black'><LinkIcon /> Demo</a>
+                </div>
               </div>
-            </div>
-          ))
-        }
+            ))
+          }
+        </div>
       </div>
     </div>
   )
