6c31b4a73d2a7f5dfc5d89a4bb568ea6b8743ac8
Gabriel Bustos <gabrielbustos0905@gmail.com>
2024-05-08 17:59:00 -0300

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,13 @@ import MenuIcon from "./icons/MenuIcon"
 import UserIcon from "./icons/UserIcon"
 import Searchbar from "./Searchbar"
 
+// ---------
+const BUTTONS = ["Wallpapers", "Naturaleza", "Viajar", "Arquitectura e Interiores", "Fotografia Callejera", "Pelicula", "Animales", "Moda y Belleza", "Gente"]
+// ---------
+
 function Header () {
     return (
-        <header className="fixed top-0 z-10 w-screen bg-white border-b-2">
+        <header className="fixed top-0 z-10 w-screen h-auto bg-white border-b-2">
             <nav className="p-2 m-auto text-wrap">
                 <div className="h-14 flex justify-between items-center p-2">
                     <div className="w-full h-full items-center flex gap-4">
@@ -18,6 +22,19 @@ function Header () {
                     </div>
                 </div>
             </nav>
+            <div className="h-10 flex items-center justify-between my-1">
+                <ul className="w-1/4 flex justify-center items-center gap-4">
+                    <li><button className="text-gray-500 text-base text-wrap hover:text-black transition">Editorial</button></li>
+                    <li><button className="text-gray-500 text-base text-wrap hover:text-black transition">Seguidos</button></li>
+                </ul>
+                <ul className="w-3/4 flex items-center justify-center gap-5 border-x-2 border-gray-400">
+                    {
+                        BUTTONS.map((button, id) => (
+                            <li key={id}><button className="text-gray-500 text-base text-wrap hover:text-black transition">{button}</button></li> 
+                        ))
+                    }
+                </ul>
+            </div>
         </header>
     )
 }
--- src/components/MoreInfo.jsx
+++ src/components/MoreInfo.jsx
@@ -3,9 +3,9 @@ import EmblaCarousel from "./carousel/EmblaCarousel"
 function MoreInfo() {
 
     return (
-        <div className="w-11/12 flex flex-col md:flex-row m-auto mt-24 justify-center items-center md:justify-between">
+        <div className="w-11/12 flex flex-col md:flex-row m-auto mt-28 justify-center items-center md:justify-between">
             <EmblaCarousel />
-            <div className="hidden lg:relative w-1/2 lg:flex flex-row-reverse">
+            <div className="hidden md:relative w-1/2 md:flex flex-row-reverse">
                 <img src="/Giant-Phone.webp"/>
                 <div className="absolute left-1 top-14 w-48 h-auto">
                     <p className="text-gray-500 text-base text-wrap"><strong className="text-xl">Gallery - Dev</strong> es una pagina en donde puedes buscar imagenes de alta calidad de todo tipo, <strong className="text-blue-300">echa un vistazo!</strong></p>
