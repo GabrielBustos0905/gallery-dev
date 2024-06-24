362894ec387a3821291e0b657d214acdd6b9b65b
Gabriel Bustos <gabrielbustos0905@gmail.com>
2024-06-24 03:20:47 -0300

    Card User

---
+++ src/components/CardUser.jsx
@@ -0,0 +1,69 @@
+import CancelIcon from "./icons/CancelIcon";
+import CheckIcon from "./icons/CheckIcon";
+import DotsIcon from "./icons/DotsIcon";
+import InstagramIcon from "./icons/InstagramIcon";
+import MailIcon from "./icons/MailIcon";
+import MapPinIcon from "./icons/MapPinIcon";
+
+function CardPhoto ({ user }) {
+
+    return (
+        <div className="flex flex-col items-center md:items-start md:flex-row w-[750px]">
+            <div>
+                <img src={user.profile_image?.large} alt="profile_image" className="rounded-full" />
+            </div>
+            <div className="ml-10 w-2/3 flex flex-col">
+                <div className="flex">
+                    <h2 className="font-bold text-4xl">{user.name}</h2>
+                    <div className="flex h-10">
+                        <button className="bg-blue-400 mx-3 p-2 rounded-md hover:bg-blue-300">
+                            <p className="text-white font-medium">Contratar</p>
+                        </button>
+                        <button className="p-2 border rounded-md shadow-md hover:border-gray-400">
+                            <MailIcon />
+                        </button>
+                        <button className="mx-2 p-1 border rounded-md shadow-md hover:border-gray-400">
+                            <DotsIcon />
+                        </button>
+                    </div>
+                </div>
+                <h4 className="text-lg text-gray-700 mt-4">{user.bio}</h4>
+                <div className="mt-10">
+                    
+                    {
+                        user?.for_hire === true ? <div className="flex items-center">
+                            <CheckIcon />
+                            <p className="ml-2 text-blue-400">Listo para contratacion</p>
+                        </div> :
+                        <div className="flex items-center">
+                            <CancelIcon />
+                            <p className="ml-2 text-red-500">No disponible para contratacion</p>
+                    </div>
+                    }
+                    <div className="my-3 flex items-center">
+                        <MapPinIcon />
+                        <p className="ml-2 text-gray-600">{user.location}</p>
+                    </div>
+                    {
+                        user.social?.instagram_username && <button className="flex items-center text-gray-600 hover:text-black">
+                            <InstagramIcon />
+                            <p className="ml-2">Instagram</p>
+                        </button>
+                    }
+                    <div className="mt-6">
+                        <p className="text-lg">Intereses</p>
+                        <div className="flex items-center gap-3 mt-2">
+                            {
+                                user.tags?.custom.map(tag => (
+                                    <p className="bg-gray-200 p-1 text-gray-700 capitalize">{tag.title}</p>
+                                ))
+                            }
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CardPhoto;
--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getUser } from "../redux/action";
+import CardUser from "../components/CardUser";
 
 function UserDetail() {
     const { username } = useParams()
@@ -13,8 +14,8 @@ function UserDetail() {
     }, [dispatch])
 
     return (
-        <div>
-            
+        <div className="mt-32">
+            <CardUser user={user}/>
         </div>
     )
 }
