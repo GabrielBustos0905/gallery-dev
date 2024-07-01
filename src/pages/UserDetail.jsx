d0985ae366282de05e5d400ba836ef0a4e6b2c74
Gabriel Bustos <gabrielbustos0905@gmail.com>
2024-07-01 03:26:04 -0300

    No c

--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -5,6 +5,7 @@ import { getUser } from "../redux/action";
 import CardUser from "../components/CardUser";
 import PhotoIcon from "../components/icons/PhotoIcon";
 import UserPhotos from "../components/UserPhotos";
+import UsersIcon from "../components/icons/UsersIcon";
 
 function UserDetail() {
     const { username } = useParams()
@@ -19,12 +20,16 @@ function UserDetail() {
         <div className="mt-32 w-full flex flex-col items-center">
             <CardUser user={user}/>
             <div className="flex w-full px-6 mt-24 gap-6">
-                <button className="flex py-1 gap-1 text-gray-500 font-medium hover:text-black border-b-2">
+                <button className="flex py-1 gap-2 text-gray-500 font-medium hover:text-black border-b-2">
                     <PhotoIcon />
                     <p className="">Fotos</p>
                 </button>
-                <button className="flex py-1 gap-1 text-gray-500 hover:text-black font-medium">
-                    <p className="">Cantidad de "Me Gusta"</p>
+                <button className="flex py-1 gap-2 text-gray-500 hover:text-black font-medium">
+                    <p className="">{user.total_likes} Me Gustas</p>
+                </button>
+                <button className="flex py-1 gap-2 text-gray-500 hover:text-black font-medium">
+                    <UsersIcon />
+                    <p className="">{user.followers_count} Seguidores</p>
                 </button>
             </div>
             <UserPhotos username={user.username}/>
